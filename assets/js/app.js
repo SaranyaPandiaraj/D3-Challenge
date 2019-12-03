99a72e4989ecc4e6d56cc81aa299390775619fa4
SaranyaPandiaraj <52775239+SaranyaPandiaraj@users.noreply.github.com>
2019-12-03 13:37:21 -0800

    Initial Commit - Adding STarter Code - HTML, JS & CSS

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
